6b046c7f28bdc3b0b8045b716a7161cd054958a9
jcccookie <doublekn88@gmail.com>
2019-02-21 23:08:16 -0800

    arror style

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 // Get local text file
 const getText = () => {
    fetch('test.txt')
-   .then(function(res){
-      return res.text();
-   })
-   .then(function(data){
+   .then(res => res.text())
+   .then(data => {
       console.log(data);
       document.getElementById('output').innerHTML = data;
    })
-   .catch(function(err){
-      console.log(err);
-   })
+   .catch(err => 
+      console.log(err)
+   )
 }
 
 //get local json data
 const getJson = () => {
    fetch('posts.json')
-   .then(function(res){
-      return res.json();
-   })
-   .then(function(data){
+   .then(res => res.json())
+   .then(data => {
       console.log(data);
       let output = '';
       data.forEach(post => {
@@ -27,18 +23,14 @@ const getJson = () => {
       });
       document.getElementById('output').innerHTML = output;
    })
-   .catch(function(err){
-      console.log(err);
-   })
+   .catch(err => console.log(err))
 }
 
 //Get from external API
 const getApi = () => {
    fetch('https://api.github.com/users')
-   .then(function(res){
-      return res.json();
-   })
-   .then(function(data){
+   .then(res => res.json())
+   .then(data => {
       console.log(data);
       let output = '';
       data.forEach(user => {
@@ -46,9 +38,7 @@ const getApi = () => {
       });
       document.getElementById('output').innerHTML = output;
    })
-   .catch(function(err){
-      console.log(err);
-   })
+   .catch(err => console.log(err));
 }
 
 document.getElementById('button1').addEventListener('click', getText);
